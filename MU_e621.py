0dada34908487edb96aa340e69a5acc25d1a3a56
z83rulek <z83rulek@users.noreply.github.com>
2019-04-29 00:04:01 +0800

    update

--- MU_e621.py
+++ MU_e621.py
@@ -12,14 +12,20 @@ ThreadCount = 16
 #URL = "https://e621.net/user/show/222675"
 #URL = sys.argv[1]
 while 1==1:
-    URL_temp = input("請輸入使用者資訊主頁的網址 (如 : https://e621.net/user/show/000001)\n>> ")
-    #print(URL_temp[0:27])
-    if URL_temp[0:27]=="https://e621.net/user/show/":
-        URL = URL_temp
-        print("格式正確")
-        break
-    else:
-        print("請檢查網址格式")
+	URL_temp = input("請輸入網址\n>> ")
+	#print(URL_temp[0:27])
+	if URL_temp[0:27]=="https://e621.net/user/show/":
+		URL = URL_temp
+		TagSearchMode = 0
+		print("格式正確")
+		break
+	elif URL_temp[0:28]=="https://e621.net/post/index/":
+		URL = URL_temp
+		TagSearchMode = 1
+		print("功能還在測試中，有錯誤請回報")
+		break
+	else:
+		print("請檢查網址格式")
 
 headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:60.0) Gecko/20100101 Firefox/60.0'}
 
@@ -27,38 +33,63 @@ rs = requests.get(URL, headers=headers)
 
 
 if rs.status_code == requests.codes.OK:
-    print("連線成功")
+	print("連線成功\n")
 else:
-    print("連線失敗\n")
-    os.system("pause")
-    os._exit(0)
+	print("連線失敗\n")
+	os.system("pause")
+	os._exit(0)
 
 html_doc = rs.text
+soup = BeautifulSoup(html_doc, 'html.parser')
 
 #print(rs) # 看HTML代碼
 #print(rs.encoding) # 看編碼格式
 #print(html_doc) # 看HTML本文
 
+if TagSearchMode == 0: #如果是從資訊主頁開始爬
+	sel = soup.select("div h2")
+	uploaderName = sel[0].text.split("\n",1)[0] #把整個字串中的第一個token拆解出來
+	folderName = uploaderName
+	sel = soup.select("table.rounded tbody tr td a")
+	totalPicture = int(sel[1].text)
+	totalPage = int(math.ceil(totalPicture / 75))
 
-soup = BeautifulSoup(html_doc, 'html.parser')
-sel = soup.select("div h2")
-uploaderName = sel[0].text.split("\n",1)[0] #把整個字串中的第一個token拆解出來
-sel = soup.select("table.rounded tbody tr td a")
-totalPicture = int(sel[1].text)
-totalPage = int(math.ceil(totalPicture / 75))
+	if uploaderName == "Users":
+		print("使用者不存在\n")
+		os.system("pause")
+		os._exit(0)
+	#上面取得了上傳者名字uploaderName跟圖片總數totalPicture跟總頁數totalPage
+elif TagSearchMode == 1: #如果是從搜尋結果開始爬
+	#要取得搜尋關鍵字，用來新增資料夾
+	searchKeywords = URL[28:].split("/",2)[1] #把網址的最後一個token拆解出來
+	folderName = searchKeywords.replace("%20", " & ") #取代字串
+	#要取得總頁數，用來更改網址、告知進度和計算圖片總數
+	sel = soup.select("div #paginator div a")
+	if len(sel) == 0:
+		totalPage = 1
+	else:
+		totalPage = int(sel[len(sel) - 2].text)
+	#要取得圖片總數，用來告知進度
+	totalPicture = 75 * (totalPage - 1) + 1
 
-if uploaderName == "Users":
-    print("使用者不存在\n")
-    os.system("pause")
-    os._exit(0)
 
-#上面取得了上傳者名字uploaderName跟圖片總數totalPicture跟總頁數totalPage
+print("總頁數 : " + str(totalPage))
+if TagSearchMode == 1:
+	print("總圖片數 : " + str(totalPicture) + " ~ " + str(totalPicture + 74))
+	print("(尚未掃描，無法得知確切數量)\n")
+else:
+	print("總圖片數 : " + str(totalPicture))
+
 
 img_page = [] #宣告list
 
-
+print("開始掃描...")
 for page in range(totalPage):
-	URL = "https://e621.net/post/index/" + str(page + 1) + "/user:" + uploaderName
+
+	if TagSearchMode == 1:
+		URL = "https://e621.net/post/index/" + str(page + 1) + "/" + str(searchKeywords)
+	else:
+		URL = "https://e621.net/post/index/" + str(page + 1) + "/user:" + uploaderName
 
 	rs = requests.get(URL, headers=headers)
 	html_doc = rs.text
@@ -76,13 +107,12 @@ for page in range(totalPage):
 
 
 numOfSub_URL = len(img_page)
-print("總頁數 : " + str(totalPage))
-print("總圖片數 : " + str(numOfSub_URL))
+print("已掃描圖片數 : " + str(numOfSub_URL))
 
-
-os.makedirs(uploaderName, exist_ok=True) #建立目錄存放檔案
-print("已新增資料夾，名為 : " + uploaderName)
-print("下載中......")
+folderName = folderName.replace(":", "：")
+os.makedirs(folderName, exist_ok=True) #建立目錄存放檔案
+print("已新增資料夾，名為 : " + folderName)
+print("\n開始下載...")
 downloadedNum = 0
 
 def downloadSomePIC(strat_num, end_num):
@@ -95,9 +125,9 @@ def downloadSomePIC(strat_num, end_num):
 		sel = soup.select("div h4 a")
 		#print(sel[0]["href"])
 		#print(sel[0]["href"].split("/")[6])
-		urllib.request.urlretrieve(sel[0]["href"], uploaderName + "/" + sel[0]["href"].split("/")[6]) #把檔名拆解出來並儲存
+		urllib.request.urlretrieve(sel[0]["href"], folderName + "/" + sel[0]["href"].split("/")[6]) #把檔名拆解出來並儲存
 		downloadedNum += 1
-		print("已下載 : " + str(downloadedNum) + "/" + str(totalPicture))
+		print("已下載 : " + str(downloadedNum) + "/" + str(len(img_page)))
 	
 numOfSub_URL = len(img_page)
 strat_num = 0
@@ -124,6 +154,8 @@ for i in range(ThreadCount):
 print("\n下載完成\n")
 os.system("pause")
 
+#要整理程式碼
+#要改成同時取所有網頁回來，再依序分析，避免等待網站回應太久
 #之後再加上檔案重複不下載的功能
 #還有可以指定頁數的範圍
 #還有接受不同格式的網址
--- readme.md
+++ readme.md
@@ -1,12 +1,12 @@
 貼上某個上傳者的個人資訊主頁的網址，就可以下載他所有的圖片、GIF和影片  
-程式會新增一個資料夾，名為圖片上傳者的使用者名稱，並且把該使用者的所有圖片都下載到裡面  
-(已使用多執行緒加速)  
+程式會新增一個資料夾，名為圖片上傳者的使用者名稱，並且把該使用者的所有圖片、GIF和影片 都下載到裡面  
+(已使用多執行緒加速下載)  
   
 執行檔下載：  
 https://github.com/z83rulek/e621_picture_downloader/releases  
   
 Paste URL of an user's home page, then you can download all of his/her pictures, GIFs and videos  
-It will create an folder named by that user's ID, and then download all picture uploaded by that user into the folder.  
+It will create an folder named by that user's ID, and then download all pictures, GIFs and videos uploaded by that user into the folder.  
 (multi-threaded program)  
   
 executable file:  
